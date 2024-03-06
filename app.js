774f2cd4f5b14ea784aa0adfb4a370f72e6c3e38
Deepak Singh <54103399+dksr789@users.noreply.github.com>
2024-03-07 01:05:01 +0530

    Update app.js
    new mongo

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/blog', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://127.0.0.1:27017/blog', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.error(err));
 
